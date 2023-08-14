05198ddac159e257a6cfae7122ff88114a386009
Salman <salmantariq@live.com>
2023-08-14 11:46:12 +0400

    Completion of 02-03 exercise with Autocomplete, Button click and webDriverWait

--- src/Ex_02_03/start/test/Autocomplete.java
+++ src/Ex_02_03/start/test/Autocomplete.java
@@ -1,7 +1,13 @@
+package Ex_02_03.start.test;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
 
 public class Autocomplete {
     public static void main(String[] args) throws InterruptedException {
@@ -10,15 +16,20 @@ public class Autocomplete {
 
         WebDriver driver = new ChromeDriver();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
         driver.get("https://formy-project.herokuapp.com/autocomplete");
 
         WebElement autocomplete = driver.findElement(By.id("autocomplete"));
-        autocomplete.sendKeys("1555 Park Blvd, Palo Alto, CA");
-        Thread.sleep(1000);
 
-        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        autocomplete.sendKeys("Al Wahda Mall Al Asalah Street");
+
+        // since the google API is no longer working, I'll test clicking the dismiss button
+        WebElement autocompleteResult = wait.until(ExpectedConditions.elementToBeClickable(By.className("dismissButton")));
+
         autocompleteResult.click();
 
+        Thread.sleep(Duration.ofSeconds(5));
         driver.quit();
     }
 }
