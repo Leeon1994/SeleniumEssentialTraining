83b28b95c4b3dcb60bfd273b634a309cfa951c77
Salman <salmantariq@live.com>
2023-08-21 13:07:27 +0400

    drag and drop exercise

--- src/Ex_02_08/start/test/DragAndDrop.java
+++ src/Ex_02_08/start/test/DragAndDrop.java
@@ -4,14 +4,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class DragAndDrop {
-    public static void main(String[] args) {
+import java.time.Duration;
+
+public class DragAndDrop  {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/dragdrop");
+
+        WebElement img = driver.findElement(By.id("image"));
+
+        WebElement box = driver.findElement(By.id("box"));
+
+        Actions actions = new Actions(driver);
+
+        actions.dragAndDrop(img,box).build().perform();
 
         driver.quit();
     }
