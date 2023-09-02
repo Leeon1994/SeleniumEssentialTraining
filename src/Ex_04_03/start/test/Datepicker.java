1d849d010638c081744efe9767130d3a713c8bc5
Salman <salmantariq@live.com>
2023-09-02 22:02:27 +0400

    Datepicker automation and using keys

--- src/Ex_04_03/start/test/Datepicker.java
+++ src/Ex_04_03/start/test/Datepicker.java
@@ -4,14 +4,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 
+import java.time.Duration;
+
 public class Datepicker {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/datepicker");
+
+        WebElement dateElement = driver.findElement(By.id("datepicker"));
+
+        dateElement.sendKeys("12/04/2022");
+
+        dateElement.sendKeys(Keys.RETURN);
 
         driver.quit();
     }
