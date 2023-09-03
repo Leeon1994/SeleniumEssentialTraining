a86fd9086de9b6bd0401af0c861f8ed5e6c95ac6
Salman <salmantariq@live.com>
2023-09-03 10:04:37 +0400

    dropdown item click testing

--- src/Ex_04_04/start/test/Dropdown.java
+++ src/Ex_04_04/start/test/Dropdown.java
@@ -3,14 +3,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 
+import java.time.Duration;
+
 public class Dropdown {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
 
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/dropdown");
+
+        WebElement dropDown = driver.findElement(By.id("dropdownMenuButton"));
+
+        dropDown.click();
+
+        WebElement dropDownItem = driver.findElement(By.id("autocomplete"));
+
+        dropDownItem.click();
 
         driver.quit();
     }
